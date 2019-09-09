b4200970073653d9e622c169c4e36a6d62571508
Charles Adeeko <cadeeko@gmail.com>
2019-09-09 09:03:48 +0100

    terraform: upate role.tf file

--- role.tf
+++ role.tf
@@ -11,22 +11,22 @@ provider "aws" {
 # Role
 
 resource "aws_iam_role" "lambda_cloudWatch_access" {
-  name = "lambda_cloudWatch_access"
+  name = "lambda_cloudWatch_access",
+  description = "lambda to access cloudwatch events and sns"
+
 
   assume_role_policy = <<EOF
 {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogGroup",
-                "logs:CreateLogStream",
-                "logs:PutLogEvents"
-            ],
-            "Resource": "*"
-        }
-    ]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
 }
 EOF
 
