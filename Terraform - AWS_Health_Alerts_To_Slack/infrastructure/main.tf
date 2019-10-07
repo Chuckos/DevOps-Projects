393e8f349e36b240c34af9e9c303ea5ee01634ef
Charles Adeeko <cadeeko@gmail.com>
2019-10-07 09:07:23 +0100

    add lambda function terraform code & update formatting & style

--- Terraform - AWS_Health_Alerts_To_Slack/infrastructure/main.tf
+++ Terraform - AWS_Health_Alerts_To_Slack/infrastructure/main.tf
@@ -5,13 +5,21 @@ provider "aws" {
   region  = "${var.aws_region}"
 }
 
+# Specify the provider to archive lambda function
+provider "archive" {}
+
+data "archive_file" "lambda_archive" {
+  type        = "zip"
+  source      = "${path.module}/lambda/lambda_aws_health_check_function.js"
+  output_path = "${path.module}/lambda/lambda_aws_health_check_function.zip"
+}
+
 # create iam lambda role policy
 resource "aws_iam_role_policy" "lambda_policy" {
-    
-    name = "${var.lambda_policy_name}"
-    role = "${aws_iam_role.lambda_cloudWatch_access.id}"
-  
-    policy = <<EOF
+  name = "${var.lambda_policy_name}"
+  role = "${aws_iam_role.lambda_cloudWatch_access.id}"
+
+  policy = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -27,12 +35,11 @@ resource "aws_iam_role_policy" "lambda_policy" {
     ]
 }
 EOF
-
 }
 
 # Create lambda role via iam service
 resource "aws_iam_role" "lambda_cloudWatch_access" {
-  name = "${var.iam_role_name}"
+  name        = "${var.iam_role_name}"
   description = "${var.iam_role_description}"
 
   assume_role_policy = <<EOF
@@ -51,22 +58,30 @@ resource "aws_iam_role" "lambda_cloudWatch_access" {
 EOF
 
   tags = {
-    cost = "evrythng"
+    cost    = "evrythng"
     project = "aws-health-alert"
   }
 }
 
-#-------------------------------
-# Lambda Function
-#-------------------------------
+# Provision lamba function
+resource "aws_lambda_function" "deploy-lambda" {
+  source        = "${path.module}/lambda/lambda_aws_health_check_function.zip"
+  filename      = "${path.module}/lambda/"
+  function_name = "health-check-sns-to-slack-test"
+  role          = "${aws_iam_role.lambda_cloudWatch_access.arn}"
+  handler       = "exports.test"
 
+  # source_code_hash = "${filebase64sha256("lambda_function_payload.zip")}"
+  source_code_hash = "${data.archive_file.lambda_archive.ouutput_base64sha256}"
+  runtime          = "nodejs10.x"
+}
 
 #-------------------------------
 # aws_cloudwatch event rule
 #-------------------------------
 resource "aws_cloudwatch_event_rule" "service_health_dashboard" {
-    name        = "capture-aws-service-health-alerts"
-    description = "Rule created to pick up any AWS personal health reports and then send to an email group via SNS topic"
+  name        = "capture-aws-service-health-alerts"
+  description = "Rule created to pick up any AWS personal health reports and then send to an email group via SNS topic"
 
   event_pattern = <<PATTERN
 {
@@ -76,5 +91,3 @@ resource "aws_cloudwatch_event_rule" "service_health_dashboard" {
 }
 PATTERN
 }
-
-
--- Terraform - AWS_Health_Alerts_To_Slack/infrastructure/variables.tf
+++ Terraform - AWS_Health_Alerts_To_Slack/infrastructure/variables.tf
@@ -1,35 +1,29 @@
 variable "aws_region" {
-    description = "AWS region to launch resources."
-    default = "us-east-1" 
+  description = "AWS region to launch resources."
+  default     = "us-east-1"
 }
 
 variable "provider_version" {
-    description = "AWS provider version to use"
-    default = "~> 2.0"
+  description = "AWS provider version to use"
+  default     = "~> 2.0"
 }
 
 variable "aws_profile" {
-    description = "AWS provider credentials in AWS config file"
-    default = "default"
+  description = "AWS provider credentials in AWS config file"
+  default     = "default"
 }
 
 variable "lambda_policy_name" {
-    description = "name of lambda policy"
-    default = "lambda_policy_for_lambda_role"
-  
+  description = "name of lambda policy"
+  default     = "lambda_policy_for_lambda_role"
 }
 
 variable "iam_role_name" {
-    description = "name of iam role for lambda"
-    default = "lambda_cloudWatch_access"
-  
+  description = "name of iam role for lambda"
+  default     = "lambda_cloudWatch_access"
 }
 
 variable "iam_role_description" {
-    description = "the role/purpose of the iam role created for lambda"
-    default = "lambda to access cloudwatch events and sns"
-  
+  description = "the role/purpose of the iam role created for lambda"
+  default     = "lambda to access cloudwatch events and sns"
 }
-
-
-
