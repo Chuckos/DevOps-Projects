70f2446a39217fcddfb775f66d4951cf2c34e645
Charles Adeeko <cadeeko@gmail.com>
2019-10-10 12:19:51 +0100

    update lambda handler path

--- Terraform - AWS_Health_Alerts_To_Slack/infrastructure/main.tf
+++ Terraform - AWS_Health_Alerts_To_Slack/infrastructure/main.tf
@@ -68,7 +68,7 @@ resource "aws_lambda_function" "deploy-lambda" {
   filename         = "${path.module}/lambda/lambda_aws_health_check_function.zip"
   function_name    = "health-check-sns-to-slack-test"
   role             = "${aws_iam_role.lambda_cloudWatch_access.arn}"
-  handler          = "exports.test"
+  handler          = "lambda_aws_health_check_function.handler"
   source_code_hash = "${data.archive_file.lambda_archive.output_base64sha256}"
   runtime          = "nodejs10.x"
 }
