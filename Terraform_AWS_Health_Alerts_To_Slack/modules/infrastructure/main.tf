2e1a7242f13027f2018f0b358c14995378abca12
Charles Adeeko <cadeeko@gmail.com>
2019-11-26 12:11:01 +0000

    remove old terraform folder

--- Terraform_AWS_Health_Alerts_To_Slack/modules/infrastructure/main.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  # common tags to be assigned to all resources
-  common_tags = {
-    Environment = "Production"
-    Project     = "aws health alert"
-    Owner       = "Charles Adeeko"
-  }
-}
