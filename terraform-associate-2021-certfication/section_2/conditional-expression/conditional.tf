1e1dec5b98129ec67ed63033f9dce25ceeefb326
Charles Adeeko <cadeeko@gmail.com>
2021-08-11 21:54:28 +0100

    add conditional expression

---
+++ terraform-associate-2021-certfication/section_2/conditional-expression/conditional.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region     = "us-west-2"
+}
+
+variable "istest" {}
+
+resource "aws_instance" "dev" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = "t2.micro"
+   count = var.istest == true ? 3 : 0
+}
+
+resource "aws_instance" "prod" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = "t2.large"
+   count = var.istest == false ? 1 : 0
+}
