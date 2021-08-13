e376309694f7fcc681c06ed15ee8b556e4f7c0eb
Charles Adeeko <cadeeko@gmail.com>
2021-08-14 00:14:28 +0100

    add load order scripts

---
+++ terraform-associate-2021-certfication/section_2/load-order/semantics.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region     = "us-west-2"
+}
+
+variable "iam_user" {
+  default = "demouser"
+}
+
+resource "aws_instance" "myec2" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = "t2.micro"
+}
+
+resource "aws_iam_user" "lb" {
+  name = var.iam_user
+  path = "/system/"
+}
