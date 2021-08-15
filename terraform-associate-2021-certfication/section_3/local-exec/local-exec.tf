a5b6ca64d6f9874ba52e6f2005ee27f2daad466f
Charles Adeeko <cadeeko@gmail.com>
2021-08-15 15:42:32 +0100

    add local-exec script

---
+++ terraform-associate-2021-certfication/section_3/local-exec/local-exec.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+resource "aws_instance" "myec2" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = "t2.micro"
+
+   provisioner "local-exec" {
+    command = "echo ${aws_instance.myec2.private_ip} >> private_ips.txt"
+  }
+}
