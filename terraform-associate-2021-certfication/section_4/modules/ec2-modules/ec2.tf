ce0006a9c8e57bedf39cded417b428c1802d529b
Charles Adeeko <cadeeko@gmail.com>
2021-08-16 22:59:10 +0100

    add module script

--- terraform-associate-2021-certfication/section_4/modules/ec2-modules/ec2.tf
+++ terraform-associate-2021-certfication/section_4/modules/ec2-modules/ec2.tf
@@ -1,6 +1,9 @@
-
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
 
 resource "aws_instance" "myec2" {
-   ami = "ami-082b5a644766e0e6f"
-   instance_type = var.instance_type
+  ami           = "ami-082b5a644766e0e6f"
+  instance_type = var.instance_type
 }
--- terraform-associate-2021-certfication/section_4/modules/project/main.tf
+++ terraform-associate-2021-certfication/section_4/modules/project/main.tf
@@ -1,8 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-  
 }
 
 module "ec2module" {
-  source = "../../modules/ec2"
+  source = "../ec2-modules/"
 }
