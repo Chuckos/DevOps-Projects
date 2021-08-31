7931042dec8c73ed6b6191d15f2e54f6d900055c
Charles Adeeko <cadeeko@gmail.com>
2021-08-31 23:52:47 +0100

    add terraform aws docker swarm scripts

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Examples of work where I have used infrastructure as code
 
 
 1. [Terraform Project - AWS Notfications](https://github.com/Chuckos/infrastructure-as-code/tree/master/Terraform_aws_notifications) - Sends AWS personal health dashboard events to a slack channel and email account.  This project uses the following resources (CloudWatch Events, SNS, Lambda, CloudFormation) to achieve this goal.
+2. [Terraform Examples - For 2021 Terraform Associate Certfication](https://github.com/Chuckos/infrastructure-as-code/tree/master/terraform-associate-2021-certfication)
+3. [Terraform Docker Swarm Example](https://github.com/Chuckos/infrastructure-as-code/tree/master/terraform-associate-2021-certfication)
---
+++ terraform-docker-swarm-aws-infrastructure/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+# Create 3 EC2 for docker swarm example
+module "ec2_instance" {
+    source = "terraform-aws-modules/ec2-instance/aws"
+    version = "~> 3.0"
+
+    for_each = toset(["manager-1", "worker-1", "worker-2"])
+
+    name = "instance-${each.key}"
+
+    ami = "ami-0c2b8ca1dad447f8a"
+    instance_type = "t2.micro"
+    key_name = "ec2-login-docker"
+
+    tags = {
+        Terraform = "true"
+        Environment = "Personal"
+        Course = "docker"
+    }
+}
+
+
+# Create 3 EC2 for docker swarm example
+# resource "aws_instance" "terraform-ec2" {
+#   ami           = "ami-0be2609ba883822ec"
+#   instance_type = "t2.micro"
+#}
